a68d75408aa45534386cb8c6757430cabb88ac7a
mithrandyr <mithrandyr@users.noreply.github.com>
2018-08-15 16:12:06 -0400

    allow from azure.

--- New-Credential.ps1
+++ New-Credential.ps1
@@ -13,13 +13,24 @@
 
 .Parameter SecurePassword
     Password for the [PSCredential], as a [securestring].
+
+.Parameter AzureKeyVault
+    Name of Azure KeyVault.
+
+.Parameter AzureSecretName
+    Name of Secret in Azure KeyVault.
 #>
 function New-Credential {
     param([Parameter(Mandatory, ValueFromPipelineByPropertyName)][string]$UserName
     , [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName="unsecure")][string]$Password
-    , [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName="secure")][securestring]$SecurePassword)
+    , [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName="secure")][securestring]$SecurePassword
+    , [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName="azure")][string]$AzureKeyVault
+    , [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName="azure")][string]$AzureSecretName)
 
     if($SecurePassword) { return [pscredential]::new($UserName, $SecurePassword) }
+    elseif($AzureKeyVault) {
+        return [pscredential]::new($UserName, (Get-AzureKeyVaultSecret -VaultName $AzureKeyVault -Name $AzureSecretName).SecretValue)
+    }
     else { return [pscredential]::new($UserName, (ConvertTo-SecureString -String $Password -AsPlainText -Force)) }
 }
 
